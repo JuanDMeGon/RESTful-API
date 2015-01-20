a91391a3fd80dbec599c38fb542d49328c0d2e36
Juan David Meza <contact@juandmegon.com>
2015-01-20 09:57:49 -0500

    Se crean los primeros controladores
    - Creamos controladores para Vehiculos y para Fabricantes

---
+++ app/Http/Controllers/FabricanteController.php
@@ -0,0 +1,86 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use App\Fabricante;
+
+class FabricanteController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return 'mostrando todod los fabricantes';
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return 'mostrando formulario para crear un fabricante';
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		return 'mostrando el fabricante con id ' . $id;
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		return 'Mostrando formulario para editar al fabricante con id '.$id;
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
---
+++ app/Http/Controllers/VehiculoController.php
@@ -0,0 +1,92 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use App\Fabricante;
+use App\Vehiculo;
+
+class VehiculoController extends Controller {
+
+	public function showAll()
+	{
+		return 'mostrando todos los vehiculos';
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index($id)
+	{
+		return 'Mostrando los vehiculos del fabricante con id '.$id;
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create($id)
+	{
+		return 'mostrando formulario para agregar vehiculo al fabricante '. $id;
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($idFabricante, $idVehiculo)
+	{
+		return "mostrando el vehiculo $idVehiculo del fabricante $idFabricante";
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($idFabricante, $idVehiculo)
+	{
+		return "Mostrando formulario para editar el vahiculo $idVehiculo del fabricante $idFabricante";
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($idFabricante, $idVehiculo)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($idFabricante, $idVehiculo)
+	{
+		//
+	}
+
+}
