e51c270784eafe3f78131e666612ec5d1c58b569
Juan David Meza <contact@juandmegon.com>
2015-01-21 18:14:19 -0500

    Se implementa el método update del recurso básico
    - Se diferencia entre PUT y PATCH para la actualización y se procede de
    manera correspondiente a cada métodos

--- app/Http/Controllers/FabricanteController.php
+++ app/Http/Controllers/FabricanteController.php
@@ -63,9 +63,50 @@ class FabricanteController extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function update($id)
+	public function update(Request $request, $id)
 	{
-		//
+		$metodo = $request->method();
+		$fabricante = Fabricante::find($id);
+
+		if(!$fabricante)
+		{
+			return response()->json(['mensaje' => 'No se encuentra este fabricante', 'codigo' => 404],404);
+		}
+
+		if($metodo === 'PATCH')
+		{
+			$nombre = $request->input('nombre');
+
+			if($nombre != null && $nombre != '')
+			{
+				$fabricante->nombre = $nombre;
+			}
+
+			$telefono = $request->input('telefono');
+
+			if($telefono != null && $telefono != '')
+			{
+				$fabricante->telefono = $telefono;
+			}
+
+			$fabricante->save();
+
+			return response()->json(['mensaje' => 'Fabricante editado'],200);
+		}
+
+		$nombre = $request->input('nombre');
+		$telefono = $request->input('telefono');
+		if(!$nombre || !$telefono)
+		{
+			return response()->json(['mensaje' => 'No se pudieron procesar los valores', 'codigo' => 422],422);
+		}
+		$fabricante->nombre = $nombre;
+		$fabricante->telefono = $telefono;
+
+		$fabricante->save();
+
+
+		return response()->json(['mensaje' => 'Fabricante editado'],200);
 	}
 
 	/**
